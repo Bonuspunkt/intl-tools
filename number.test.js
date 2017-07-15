f03aa647583daf5bde5c880a744116ddfcf44cf2
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-07-15 23:00:58 +0200

    try qunit

---
+++ number.test.js
@@ -0,0 +1,61 @@
+const QUnit = require('qunitjs')
+const number = require('./number');
+
+QUnit.module('number', hooks => {
+
+    ['de', 'de-AT', 'ko', 'jp', 'hi-IN'].forEach(locale => {
+
+        QUnit.test(`parse '${ locale }'`, assert => {
+            const numberFormat = new Intl.NumberFormat(locale, {
+                useGrouping: true,
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 3
+            });
+
+            [
+                123.456,
+                -123.456,
+                1234.56,
+                -1234.56
+            ].forEach(expected => {
+                const string = numberFormat.format(expected);
+                const actual = number.parse(string, locale)
+                assert.equal(actual, expected);
+            });
+
+            assert.equal(number.parse('', locale), null);
+
+            assert.ok(Number.isNaN(number.parse('1a', locale)));
+        });
+
+        QUnit.test(`scientific '${ locale }'`, assert => {
+            const numberFormat = new Intl.NumberFormat(locale, {
+                minimumSignificantDigits: 2,
+                maximumSignificantDigits: 2
+            });
+            const format = number => numberFormat.format(number);
+
+            assert.equal(number.parse(`${ format(1.3) }e5`, locale), 1.3e5);
+            assert.equal(number.parse(`${ format(-1.3) }e5`, locale), -1.3e5);
+            assert.equal(number.parse(`${ format(1.3) }e+5`, locale), 1.3e5);
+            assert.equal(number.parse(`${ format(1.3) }e-5`, locale), 1.3e-5);
+        });
+
+    });
+
+
+    QUnit.test('significantDigits', assert => {
+        assert.equal(number.significantDigits(0), 0);
+        assert.equal(number.significantDigits(2), 1);
+        assert.equal(number.significantDigits(1234), 4);
+        assert.equal(number.significantDigits(2.34), 3);
+        assert.equal(number.significantDigits(3000), 1);
+        assert.equal(number.significantDigits(0.0034), 2);
+        assert.equal(number.significantDigits(120.5e50), 4);
+        assert.equal(number.significantDigits(1120.5e+50), 5);
+        assert.equal(number.significantDigits(120.52e-50), 5);
+        assert.equal(number.significantDigits(Math.PI), 16);
+    });
+});
+
+QUnit.start();
---
+++ test/index.js
@@ -0,0 +1,61 @@
+const chromeLauncher = require('chrome-launcher');
+const CDP = require('chrome-remote-interface');
+
+
+function wait(duration = 1e3) {
+    return new Promise(resolve => setTimeout(resolve, duration));
+}
+
+
+chromeLauncher.launch({
+    logLevel: 'silent',
+    chromeFlags: [
+        '--disable-gpu',
+        '--headless'
+    ]
+}).then(async chrome => {
+    const protocol = await CDP({ port: chrome.port });
+    const { Page, DOM } = protocol;
+
+    await Page.enable();
+    await DOM.enable();
+
+    Page.navigate({ url: 'http://localhost:8080/number.html' })
+
+    await new Promise((resolve, reject) => {
+        setTimeout(reject, 30e3);
+
+        Page.loadEventFired(async () => {
+
+            const { root: { nodeId } } = await DOM.getDocument();
+
+            let bannerNodeId = {};
+            while (!bannerNodeId.nodeId) {
+                await wait();
+
+                bannerNodeId = await DOM.querySelector({ nodeId, selector: '#qunit-banner' });
+            }
+            const banner = await DOM.resolveNode(bannerNodeId);
+            const { attributes } = await DOM.getAttributes(bannerNodeId);
+
+
+            if (attributes.includes('qunit-pass')) {
+                resolve();
+            } else {
+                reject();
+            }
+        });
+    })
+    .then(() => {
+        console.log('success');
+    })
+    .catch(() => {
+        console.log('failed');
+        process.exit(1);
+    })
+    .then(() => {
+        protocol.close();
+        chrome.kill();
+    });
+
+});
